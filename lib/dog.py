d33be82e9a7c20868db5212dcc4497069e4bcbc0
Emmanuel Mutugi <smontana025@gmail.com>
2024-12-13 18:15:02 +0300

    mannuh

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
-
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name  # Name of the dog
+        self.breed = breed  # Breed of the dog, defaulting to "Mutt"
+
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
